2f29a752664da0e4128c3b46dc6e33004b38e5a7
kanhiya chowhan <kanhiyachowhan197@gmail.com>
2026-09-09 20:34:25 +0530

    Time: 17 ms (92.28%), Space: 21.8 MB (88.9%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -0,0 +1,22 @@
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.curr = 0
+        self.bound = 0
+
+    def visit(self, url: str) -> None:
+        self.curr += 1
+        if self.curr < len(self.history):
+            self.history[self.curr] = url
+        else:
+            self.history.append(url)
+        self.bound = self.curr
+
+    def back(self, steps: int) -> str:
+        self.curr = max(0, self.curr - steps)
+        return self.history[self.curr]
+
+    def forward(self, steps: int) -> str:
+        self.curr = min(self.bound, self.curr + steps)
+        return self.history[self.curr]
